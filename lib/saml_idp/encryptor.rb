2651d437c1d3c3df4de0ea1f602f718836a91831
Mitchell Henke <mitchell.henke@gsa.gov>
2024-12-03 16:56:58 -0600

    Add support for AES-GCM encryption algorithms

--- lib/saml_idp/encryptor.rb
+++ lib/saml_idp/encryptor.rb
@@ -34,8 +34,25 @@ module SamlIdp
     end
     private :openssl_cert
 
+    # Encryption algorithms enumerated in:
+    # https://github.com/digidentity/xmlenc/blob/937ca2f/lib/xmlenc/encrypted_data.rb#L3-L10
     def block_encryption_ns
-      "http://www.w3.org/2001/04/xmlenc##{block_encryption}"
+      case block_encryption
+        when 'tripledes-cbc'
+          'http://www.w3.org/2001/04/xmlenc#tripledes-cbc'
+        when 'aes128-cbc'
+        'http://www.w3.org/2001/04/xmlenc#aes128-cbc'
+        when 'aes256-cbc'
+          'http://www.w3.org/2001/04/xmlenc#aes256-cbc'
+        when 'aes128-gcm'
+          'http://www.w3.org/2009/xmlenc11#aes128-gcm'
+        when 'aes192-gcm'
+          'http://www.w3.org/2009/xmlenc11#aes192-gcm'
+        when 'aes256-gcm'
+          'http://www.w3.org/2009/xmlenc11#aes256-gcm'
+        else
+          "http://www.w3.org/2009/xmlenc11##{block_encryption}"
+      end
     end
     private :block_encryption_ns
 
--- spec/lib/saml_idp/encryptor_spec.rb
+++ spec/lib/saml_idp/encryptor_spec.rb
@@ -32,5 +32,26 @@ module SamlIdp
 
       expect(encrypted_doc.remove_namespaces!.xpath('//KeyName')).to be_empty
     end
+
+    context 'gcm encryption algorithm' do
+      let(:encryption_opts) do
+        {
+          cert: Default::X509_CERTIFICATE,
+          block_encryption: 'aes256-gcm',
+          key_transport: 'rsa-oaep-mgf1p',
+        }
+      end
+
+      it 'encrypts XML' do
+        raw_xml = '<foo>bar</foo>'
+        encrypted_xml = subject.encrypt(raw_xml)
+        expect(encrypted_xml).not_to match 'bar'
+        encrypted_doc = Nokogiri::XML::Document.parse(encrypted_xml)
+        encrypted_data = Xmlenc::EncryptedData.new(encrypted_doc.at_xpath('//xenc:EncryptedData',
+                                                                          Xmlenc::NAMESPACES))
+        decrypted_xml = encrypted_data.decrypt(subject.encryption_key)
+        expect(decrypted_xml).to eq(raw_xml)
+      end
+    end
   end
 end
